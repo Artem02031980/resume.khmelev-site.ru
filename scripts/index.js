c7c6949effccd9c9f93dd2a9d3360d6bf3d87a13
Artem Khmelev <artem0203@yandex.ru>
2023-02-08 10:54:31 +0300

    feature: header

---
+++ scripts/index.js
@@ -0,0 +1,63 @@
+//глобальные переменные
+const header = document.querySelector('.header');
+const hero = document.querySelector('.hero');
+
+//плавный скролл
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+  link.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      let href = this.getAttribute('href').substring(1);
+
+      const scrollTarget = document.getElementById(href);
+
+      const topOffset = header.offsetHeight;
+      //const topOffset = 0; // если не нужен отступ сверху
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      window.scrollBy({
+          top: offsetPosition,
+          behavior: 'smooth'
+      });
+  });
+});
+
+//появление стики-хедера при скролле
+document.addEventListener('DOMContentLoaded', () => {
+
+  window.addEventListener('scroll', () => {
+
+    let scrollTop = window.scrollY;
+    let heroCenter = hero.offsetHeight / 2;
+
+    if (scrollTop >= heroCenter) {
+      header.classList.add('header_sticky')
+      hero.style.marginTop = header.offsetHeight;
+    }  else {
+      header.classList.remove('header_sticky')
+      hero.style.marginTop = '0px';
+    };
+
+  });
+});
+
+//хедер-бургер
+const headerBurger = document.querySelector('.header__burger');
+const headerLinks = document.querySelector('.header__links');
+const headerLink = document.querySelectorAll('.header__link');
+
+headerBurger.addEventListener('click', () => {
+  document.body.classList.toggle('stop-scroll');
+  headerBurger.classList.toggle('header__burger_active');
+  headerLinks.classList.toggle('header__links_visible');
+});
+
+headerLink.forEach(el => {
+  el.addEventListener('click', () => {
+    document.body.classList.remove('stop-scroll');
+    headerBurger.classList.remove('header__burger_active');
+    headerLinks.classList.remove('header__links_visible');
+  });
+});
