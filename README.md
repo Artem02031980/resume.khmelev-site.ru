58cf26645ffefd5ef2500bb07c18d4ca8099e077
Artem Khmelev <artem0203@yandex.ru>
2023-09-04 13:06:19 +0300

    version 2.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # https://artem02031980.github.io/resume.khmelev-site.ru/
-Website-resume of a front-end-developer and web-designer
+Website-resume of a web-designer (UI/UX, graphics, html-coding)
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,78 +1,20 @@
-//повторяющиеся переменные
-const header = document.querySelector('.header');
+//актуальный год
+const currentYear = document.getElementById('currentYear');
+currentYear.innerHTML = new Date().getFullYear();
 
-//плавный скролл
-document.querySelectorAll('a[href^="#"').forEach(link => {
-
-  link.addEventListener('click', function(e) {
-      e.preventDefault();
-
-      let href = this.getAttribute('href').substring(1);
-
-      const scrollTarget = document.getElementById(href);
-
-      const topOffset = header.offsetHeight;
-      //const topOffset = 0; // если не нужен отступ сверху
-      const elementPosition = scrollTarget.getBoundingClientRect().top;
-      const offsetPosition = elementPosition - topOffset;
-
-      window.scrollBy({
-          top: offsetPosition,
-          behavior: 'smooth'
-      });
-  });
-});
-
-//появление стики-хедера при скролле
-const hero = document.querySelector('.hero');
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  window.addEventListener('scroll', () => {
-
-    let scrollTop = window.scrollY;
-    let heroCenter = hero.offsetHeight / 2;
-
-    if (scrollTop >= heroCenter) {
-      header.classList.add('header_sticky')
-      hero.style.marginTop = header.offsetHeight;
-    }  else {
-      header.classList.remove('header_sticky')
-      hero.style.marginTop = '0px';
-    };
-
-  });
-});
-
-//хедер-бургер
-const headerBurger = document.querySelector('.header__burger');
-const headerLinks = document.querySelector('.header__links');
-const headerLink = document.querySelectorAll('.header__link');
-
-headerBurger.addEventListener('click', () => {
-  document.body.classList.toggle('stop-scroll');
-  headerBurger.classList.toggle('header__burger_active');
-  headerLinks.classList.toggle('header__links_visible');
-});
-
-headerLink.forEach(el => {
-  el.addEventListener('click', () => {
-    document.body.classList.remove('stop-scroll');
-    headerBurger.classList.remove('header__burger_active');
-    headerLinks.classList.remove('header__links_visible');
-  });
-});
+//custom-scroll
+Array.prototype.forEach.call(
+    document.querySelectorAll('.custom-scroll'),
+    el => new SimpleBar()
+);
 
 //актуальный возраст
 const currentAge = document.getElementById('currentAge');
+const currentAgeMobile = document.getElementById('currentAgeMobile');
 
 let birth = new Date(1980, 2, 2),
     year = birth.getFullYear(),
     today = new Date();
 
 currentAge.innerHTML = today.getFullYear() - year - (today.getTime() < birth.setFullYear(year));
-
-
-//актуальный год
-const currentYear = document.getElementById('currentYear');
-currentYear.innerHTML = new Date().getFullYear();
+currentAgeMobile.innerHTML = today.getFullYear() - year - (today.getTime() < birth.setFullYear(year));
